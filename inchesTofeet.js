94d3732052aeb4a1d84714d6b9603ee4a6525e5e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-05 02:02:14 +0000

    converstion

---
+++ inchesTofeet.js
@@ -0,0 +1,6 @@
+function inchesTofeet(inches){
+    var feet=inches/12;
+    return feet;
+}
+var result=inchesTofeet(24);
+console.log(result);
---
+++ milesTokm.js
@@ -0,0 +1,13 @@
+function milesTokm(miles){
+    var km=miles*1.60934;
+    return km;
+}
+const result=3;
+milesTokm(result);
+
+
+function milesTokm(miles){
+    var km=miles*1.60934;
+    console.log(km);
+}
+milesTokm(3);
