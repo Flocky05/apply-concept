ad70a8eb21b080917987efacbb77834759ce51d3
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-05 02:21:58 +0000

    added odd and even number

---
+++ odd_even.js
@@ -0,0 +1,9 @@
+function odd_even(number){
+    if(number%2==0){
+        return true;
+     }else{
+         return false;
+     }
+}
+let myNumberIsEven=odd_even(342);
+console.log(myNumberIsEven);
