5b25b09ac7ae4bbf7d0f141c8e1b81b75091cbd8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-05 15:59:55 +0000

    added sumation of seris

---
+++ sumOfseris.js
@@ -0,0 +1,12 @@
+function sumOfseris(array){
+    var sum=0;
+    for(var i=0;i<array.length;i++){
+    
+        sum=sum+array[i];
+        
+        
+    }
+    console.log("The sumation of seris is:",sum);
+}
+var array=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+sumOfseris(array)
